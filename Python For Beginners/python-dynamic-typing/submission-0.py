c340f2e0fa9416069746dc9d80a65fda9e5f6f7f
Yuuki11 <91509011+Yuuki11@users.noreply.github.com>
2026-04-19 16:58:40 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 0
+print(type(variable))
+variable = 0.1
+
+print(type(variable))
+variable =  True
+
+print(type(variable))
+variable = "0"
+
+print(type(variable))
+variable = ["0", "0"]
+
+print(type(variable))
